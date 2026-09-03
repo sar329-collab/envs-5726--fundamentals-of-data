068c09054e47b1aed473063d4e8642948ea43f30
Shaun <sar329@cornell.edu>
2026-09-03 13:03:02 -0400

    Created first python file

--- Week 2/organizations.py
+++ Week 2/organizations.py
@@ -1,5 +1,5 @@
 class Company:
-    def__init__(self, name, market_cap, share_value):
+    def __init__(self, name, market_cap, share_value):
         self.name=name
         self.market_cap
         self.share_value
@@ -8,8 +8,7 @@ class Company:
         return self.market_cap/self.share_value
         
 class NonProfit:
-    def__init__(self, name, assets, designation):
+    def __init__(self, name, assets, designation):
         self.name=name
         self.assets=assets
         self.designation=designation
-        
--- Week 2/step 2.py
+++ Week 2/step 2.py
@@ -1,6 +1,10 @@
 import sys
-sys.path.insert(_index 1, _object: /Users/shaunroberts/PycharmProjects/envs-5726--fundamentals-of-data/Week\ 2/organizations.py)
+from operator import index
+
+sys.path.insert(index(1),object r'C:\Users/shaunroberts/PycharmProjects/envs-5726--fundamentals-of-data/Week\ 2/organizations.py')
+
 import  organizations
+
 american_water=organizations.company(name='American Water', market_cap=28940000000, share_value=148.4)
 american_water_shares=american_water.get_number_of_share()
 print(american_water_shares)
