59836166c4ee19f3045bb0837df48eed3c964662
Shaun <sar329@cornell.edu>
2026-09-03 12:55:29 -0400

    Created first python file

---
+++ Week 2/organizations.py
@@ -0,0 +1,15 @@
+class Company:
+    def__init__(self, name, market_cap, share_value):
+        self.name=name
+        self.market_cap
+        self.share_value
+        
+    def get_number_of_shares(self):
+        return self.market_cap/self.share_value
+        
+class NonProfit:
+    def__init__(self, name, assets, designation):
+        self.name=name
+        self.assets=assets
+        self.designation=designation
+        
---
+++ Week 2/step 2.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(_index 1, _object: /Users/shaunroberts/PycharmProjects/envs-5726--fundamentals-of-data/Week\ 2/organizations.py)
+import  organizations
+american_water=organizations.company(name='American Water', market_cap=28940000000, share_value=148.4)
+american_water_shares=american_water.get_number_of_share()
+print(american_water_shares)
+
+partnership_of_delaware_estuary=organizations.Nonprofit(name='Partnerships of the Delaware Estuary', assets = 3140000, designations = '501c3')
+
+print(partnership_of_delaware_estuary.name)
+print(partnership_of_delaware_estuary.assets)
+print(partnership_of_delaware_estuary.designations)
