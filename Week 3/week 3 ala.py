5af4dbeb1f0d566890f339a9a097468d867ad26a
Shaun <sar329@cornell.edu>
2026-09-10 12:14:31 -0400

    Created first python file

--- Week 3/week 3 ala.py
+++ Week 3/week 3 ala.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import statistics
+import datetime
+def is_min_ratio_toilets_to_people_met(ratio_of_toilets):
+    if ratio_of_toilets >= 1/20:
+        return True
+    else:
+        return False
+print(is_min_ratio_toilets_to_people_met('1t/37p'))
+print(is_min_ratio_toilets_to_people_met('1t/12p'))# This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def is_population_disabled(disabled, total_population):
+  return disabled / total_population
+if(is_population_disabled)< .10:
+    return ("False")
+if(is_population_disabled)> .10:
+    return ("True")
+print(is_population_disabled(disabled=0, total_population=32))
+print(is_population_disabled(disabled=52, total_population=392))
