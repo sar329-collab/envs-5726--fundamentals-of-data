50c220d86d9dcb75dd3eb6748a1de4312b817056
Shaun <sar329@cornell.edu>
2026-09-10 11:34:45 -0400

    Created first python file

--- Week 2/PreySample V3.py
+++ Week 2/PreySample V3.py
@@ -1,5 +1,5 @@
-import datetime
 import statistics
+import datetime
 
 class PreySample:
     def __init__(self, full_species_name, delta13c_list, tissue_description, sample_date_utc):
@@ -10,24 +10,38 @@ class PreySample:
 
     def get_common_name(self):
         return self.full_species_name.split(" (")[0]
+
     def get_scientific_name(self):
-        return self.full_species_name.split(")")[1]
+        return self.full_species_name.split(" (")[1].replace(" )","").split(")")[0].replace(" )", "")
+
     def get_average_delta13c(self):
         return statistics.mean(self.delta13c_list)
+
     def get_tissue_count(self):
-        return self.len(self.tissue_description)
+        return len(self.tissue_description.split(","))
+
     def get_sample_date(self):
-        return self.datetime.datetime.strptime(self.sample_date_utc, "%Y-%m-%d %H:%M:%S")
+        return datetime.datetime.strptime(self.sample_date_utc, "%Y-%m-%dT%H:%M:%SZ")
+
+    def get_discrimination_factor(self, predator_delta13c: float):
+        return predator_delta13c - self.get_average_delta13c()
 
 sample1 = PreySample(
-    full_species_name = "Harbor Seal (Phoca vitulina",
+    full_species_name = "Harbor Seal (Phoca vitulina)",
     delta13c_list = [-12.4, -11.3, -10.6, -13.5, -15.8],
     tissue_description = "Bone collagen, Bone collagen, Muscle, Skin",
-    sample_date_utc ='2020-11-16T04:25:032'
+    sample_date_utc ='2020-11-16T04:25:03Z'
 )
 print(sample1.get_common_name())
 print(sample1.get_scientific_name())
 print(sample1.get_average_delta13c())
-print(sample1.get_average_delta13c())
+print(sample1.get_tissue_count())
 print(sample1.get_sample_date())
 
+Sample2 = PreySample(
+    full_species_name = "Pacific pomrat (Brama japonica",
+    delta13c_list= [-16.1,-17.8,-19.6,-19.1,-18.0],
+    tissue_description= "Whole animal",
+    sample_date_utc ='2020-11-17T05:00:02Z',
+)
+print(Sample2.get_discrimination_factor(predator_delta13c=-15.5))
