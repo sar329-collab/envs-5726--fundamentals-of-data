d082c7a8532947f28dd51207ae16834a5a5c28a5
Shaun <sar329@cornell.edu>
2026-09-18 17:23:52 -0400

    Created first python file

--- Week 4/Week 4 ALA.py
+++ Week 4/Week 4 ALA.py
@@ -1,17 +1,95 @@
-from    pathlib import Path
-folder_path = Path(r'/Users/shaunroberts/Desktop/ENVS\ 5726/SEC_EDGAR_10K')
-for file_path in folder_path.glob('*'):
-    print(file_path)
+import csv
+from pathlib import Path
+from statistics import mean
 
-file_headers = ['Company Name', 'Year', 'Count Sustainability','Count AI']
-file_table = []
-for file_path in folder_path.glob('*'):
+sec_path = Path(r'/Users/shaunroberts/Desktop/ENVS 5726/SEC_EDGAR_10K')
+
+sec_headers = ['Company Name', 'Year', 'Count Sustainability','Count AI']
+sec_table = []
+
+for file_path in sec_path.glob('*'):
     file_name = file_path.name
-    file_extention = file_name.split('.')[-1]
-    file_table.append([str(file_path), file_name, file_extention])
+    name_list = file_path.name.split('-')
 
-print(file_headers)
-for row in file_table:
+    if name_list[0] == 'amzn':
+        Company_Name = 'Amazon'
+        Year_part = name_list[1][0:4]
+
+    elif name_list[0] == 'goog':
+        Company_Name = 'Google'
+        Year_part = name_list[1][0:4]
+
+    elif name_list[0] == 'msft':
+        Company_Name = 'Microsoft'
+        Year_part = name_list[1][4:8]
+    else:
+        Company_Name = 'Nvidia'
+        Year_part = name_list[1][0:4]
+
+    with open(file_path, 'r', encoding='cp1252') as file:
+        content = file.read().upper()
+    Count_Sustainability = content.count('SUSTAINABILITY')
+    Count_AI = content.count('ARTIFICIAL INTELLIGENCE')
+
+    sec_table.append([Company_Name, Year_part, Count_Sustainability, Count_AI])
+print(sec_headers)
+for row in sec_table:
     print(row)
 
+import statistics
+def get_average_by_company(headers,table,column_name_to_average, company_name):
+    values_to_average= []
+    for row in table:
+        if row[0] == company_name:
+            value = row[headers.index(column_name_to_average)]
+            values_to_average.append(value)
+    column_average = statistics.mean(values_to_average)
+    return column_average
 
+for company_name in ['Nvidia','Microsoft','Google','Amazon']:
+    for column_name_to_average in ['Count Sustainability', 'Count AI']:
+        column_average = get_average_by_company(headers= sec_headers,
+                                                table= sec_table,
+                                                column_name_to_average=column_name_to_average,
+                                                company_name=company_name)
+        print(f'The average of {column_name_to_average} for {company_name} is: {column_average}')
+
+amazon_metric_table = []
+for row in sec_table:
+    if row[sec_headers.index('Company Name')] == 'Amazon':
+        amazon_metric_table.append(row)
+
+export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726/SEC_10k_Amazon_Metrics.csv'))
+with open(export_csv_path, 'w', newline='', encoding='cp1252') as file:
+    writer = csv.writer(file)
+    writer.writerows([sec_headers]+amazon_metric_table)
+
+google_metric_table = []
+for row in sec_table:
+    if row[sec_headers.index('Company Name')] == 'Google':
+        google_metric_table.append(row)
+
+export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726/SEC_10k_Google_Metrics.csv'))
+with open(export_csv_path, 'w', newline='', encoding='cp1252') as file:
+    writer = csv.writer(file)
+    writer.writerows([sec_headers]+google_metric_table)
+
+Microsoft_metric_table = []
+for row in sec_table:
+    if row[sec_headers.index('Company Name')] == 'Microsoft':
+        Microsoft_metric_table.append(row)
+
+export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726/SEC_10k_Microsoft_Metrics.csv'))
+with open(export_csv_path, 'w', newline='', encoding='cp1252') as file:
+    writer = csv.writer(file)
+    writer.writerows([sec_headers]+Microsoft_metric_table)
+
+Nvidia_metric_table = []
+for row in sec_table:
+    if row[sec_headers.index('Company Name')] == 'Nvidia':
+        Nvidia_metric_table.append(row)
+
+export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726/SEC_10k_Nvidia_Metrics.csv'))
+with open(export_csv_path, 'w', newline='', encoding='cp1252') as file:
+    writer = csv.writer(file)
+    writer.writerows([sec_headers]+Nvidia_metric_table)
--- Week 4/week 4 test.py
+++ Week 4/week 4 test.py
@@ -1,10 +1,47 @@
-file_headers = ['Company Name', 'Year', 'Count Sustainability','Count AI']
-file_table = []
-for file_path in folder_path.glob('*'):
-    file_name = file_path.name
-    file_extention = file_name.split('.')[-1]
-    file_table.append([str(file_path), file_name, file_extention])
+import csv
+from pathlib import Path
+from statistics import mean
 
-print(file_headers)
-for row in file_table:
+sec_path = Path(r'/Users/shaunroberts/Desktop/ENVS 5726/SEC_EDGAR_10K')
+
+sec_headers = ['Company Name', 'Year', 'Count Sustainability','Count AI']
+sec_table = []
+
+for file_path in sec_path.glob('*'):
+    file_name = file_path.name
+    name_list = file_path.name.split('-')
+
+    if name_list[0] == 'amzn':
+        Company_Name = 'Amazon'
+        Year_part = name_list[1][0:4]
+
+    elif name_list[0] == 'goog':
+        Company_Name = 'Google'
+        Year_part = name_list[1][0:4]
+
+    elif name_list[0] == 'msft':
+        Company_Name = 'Microsoft'
+        Year_part = name_list[1][4:8]
+    else:
+        Company_Name = 'Nvidia'
+        Year_part = name_list[1][0:4]
+
+    with open(file_path, 'r', encoding='cp1252') as file:
+        content = file.read().upper()
+    Count_Sustainability = content.count('SUSTAINABILITY')
+    Count_AI = content.count('ARITIFICIAL INTELLIGENCE')
+
+    sec_table.append([Company_Name, Year_part, Count_Sustainability, Count_AI])
+print(sec_headers)
+for row in sec_table:
     print(row)
+
+amazon_metric_table = []
+for row in sec_table:
+    if row[sec_headers.index('Company Name')] == 'Amazon':
+        amazon_metric_table.append(row)
+
+export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726')
+    with open(export_csv_path, 'w',newline="", encoding=)
+        writer = csv.writer(file)
+        writer.writerows([sec_headers]+amazon_metric_table)
