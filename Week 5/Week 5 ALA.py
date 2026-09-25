5ab55be779486f656907922d4e1a4e76ffaa2220
Shaun <sar329@cornell.edu>
2026-09-25 09:54:29 -0400

    Created first python file

--- Week 5/Week 5 ALA.py
+++ Week 5/Week 5 ALA.py
@@ -37,9 +37,9 @@ with open(csv_path, 'r', encoding='cp1252') as csv_file:
         trader_group, country_of_destination, cocoa_deforestation_15_years_total_exposure, cocoa_net_emissions_15_years_total = row
 
         if country_of_destination not in Countries_by_deforestation_dict:
-            Countries_by_deforestation_dict[country_of_destination] = [cocoa_deforestation_15_years_total_exposure]
+            Countries_by_deforestation_dict[country_of_destination] = [float(cocoa_deforestation_15_years_total_exposure)]
         else:
-            Countries_by_deforestation_dict[country_of_destination].append(cocoa_deforestation_15_years_total_exposure)
+            Countries_by_deforestation_dict[country_of_destination].append(float(cocoa_deforestation_15_years_total_exposure))
 
 export_json_path = Path(r'/Users/shaunroberts/Desktop/ENVS 5726/week 5/Countries_by_deforestation')
 with open(export_json_path, 'w') as json_file:
@@ -56,9 +56,9 @@ with open(csv_path, 'r', encoding='cp1252') as csv_file:
         trader_group, country_of_destination, cocoa_deforestation_15_years_total_exposure, cocoa_net_emissions_15_years_total = row
 
         if country_of_destination not in Countries_by_net_emissions_dict:
-            Countries_by_net_emissions_dict[country_of_destination] = [cocoa_net_emissions_15_years_total]
+            Countries_by_net_emissions_dict[country_of_destination] = [float(cocoa_net_emissions_15_years_total)]
         else:
-            Countries_by_net_emissions_dict[country_of_destination].append(cocoa_net_emissions_15_years_total)
+            Countries_by_net_emissions_dict[country_of_destination].append(float(cocoa_net_emissions_15_years_total))
 
 export_json_path = Path(r'/Users/shaunroberts/Desktop/ENVS 5726/week 5/Countries_by_net_emissions')
 with open(export_json_path, 'w') as json_file:
@@ -66,14 +66,15 @@ with open(export_json_path, 'w') as json_file:
 
 def get_summary_data(summary_dict,):
     summary_table = []
+    max_list = []
+
 
     for country_of_destination in summary_dict:
         values = summary_dict[country_of_destination]
-        total = sum(float(values))
-        summary_table.append([country_of_destination, total])
-        total.append(country_of_destination)
+        total = sum(values)
+        max_list.append(total)
 
-    max_sum = max(total)
+    max_sum = max(max_list)
     threshold = max_sum * .1
 
     for country_of_destination in summary_dict:
@@ -91,10 +92,10 @@ summary_emissions = get_summary_data(Countries_by_net_emissions_dict)
 export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726/week 5/Summary_Deforestation.csv'))
 with open(export_csv_path, 'w', newline='', encoding='cp1252') as file:
     writer = csv.writer(file)
-    writer.writerows([data_headers]+summary_deforestation)
+    writer.writerows([['Country', 'Deforestation']]+summary_deforestation)
 
 export_csv_path = (Path(r'/Users/shaunroberts/Desktop/ENVS 5726/week 5/Summary_Net_Emissions.csv'))
 with open(export_csv_path, 'w', newline='', encoding='cp1252') as file:
     writer = csv.writer(file)
-    writer.writerows([data_headers]+summary_emissions)
+    writer.writerows([['Country', 'Emissions']]+summary_emissions)
 
